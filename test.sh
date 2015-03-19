31ef6fe0c6afa731b16be824423fa68a97055265
Jason Garland <jason_garland@gap.com>
2015-03-18 17:30:02 -0700

    adding test script

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+olddir=$(pwd)
+
+repo="https://github.com/jgarland79/sandbox2.git"
+file="README.md"
+
+rm -rf /tmp/foo
+mkdir /tmp/foo
+cd /tmp/foo
+git clone -n ${repo} --depth 1 /tmp/foo
+
+git checkout HEAD ${file}
+
+git status
+
+date >>${file}
+openssl rand -hex 4 >>${file}
+echo >>${file}
+
+git add ${file}
+
+git commit -m 'added date and random data'
+
+git push
+
+cd ${olddir}
