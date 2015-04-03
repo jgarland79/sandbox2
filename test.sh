5c2212079ceb47e23204cf341f4833d705e3a24b
Jason Garland <jason_garland@gap.com>
2015-04-03 16:00:30 -0700

    script to create new branch and pull request

--- test.sh
+++ test.sh
@@ -2,17 +2,27 @@
 
 olddir=$(pwd)
 
-repo="https://github.com/jgarland79/sandbox2.git"
+
+git_user='jgarland79'
+git_pass='password'
+git_api='https://api.github.com'
+git_web='https://github.com'
+git_repo='sandbox2'
+
+repo="${git_web}/${git_user}/${git_repo}.git"
 file="README.md"
+
+branch=$(date '+%Y%m%d%H%M%S')
+
 
 rm -rf /tmp/foo
 mkdir /tmp/foo
 cd /tmp/foo
 git clone -n ${repo} --depth 1 /tmp/foo
 
-git checkout --ignore-skip-worktree-bits HEAD ${file}
+git checkout HEAD ${file}
 
-git status
+#base_sha=$(git log -n 1 |head -n 1 |awk '{print $2}')
 
 date >>${file}
 openssl rand -hex 4 >>${file}
@@ -20,11 +30,43 @@ echo >>${file}
 
 git reset HEAD ./
 
+echo Switching to ${branch}
+git checkout -b ${branch}
+#git branch ${branch}
+
 git add ${file}
+
+git status
 
 git commit -m 'added date and random data'
 
-git push
+echo Pushing to ${branch}
+git push -u origin "${branch}"
+
+#head_sha=$(git log --branches ${branch} -n 1 |head -n 1 |awk '{print $2}')
+
+json="{
+\"title\": \"Amazing new feature\",
+\"body\": \"Please pull this in!\",
+\"head\": \"${branch}\",
+\"base\": \"master\"
+}"
+
+#json="{
+#\"title\": \"Amazing new feature\",
+#\"body\": \"Please pull this in!\",
+#\"head\": \"${branch}\",
+#\"head_sha\": \"${head_sha}\",
+#\"base\": \"master\",
+#\"base_sha\": \"${base_sha}\"
+#}"
+
+
+echo "${json}"
+
+curl -u "${git_user}:${git_pass}" \
+  -XPOST "${git_api}/repos/${git_user}/${git_repo}/pulls" \
+  -d "${json}"
 
 cd ${olddir}
 
